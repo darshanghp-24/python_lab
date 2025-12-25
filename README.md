d6c6fcfd7153436a79cc719afbd66691329785cc
darshanghp-24 <darshang24hp@gmail.com>
2025-12-25 08:43:04 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # python_lab
 A lab for coding, testing, and discovering new things in python.
 <br>
-Python Programming
+Python Programming.
---
+++ bandnamegenerator.py
@@ -0,0 +1,6 @@
+# Project 01
+# Bandname Generator
+print("Welcome to the Bandname Generator.")
+city = input("What is the name of your favourite city?\n")
+name = input("What is your favourite name?\n")
+print("Your Bandname will be" + " " + city + " " + name)
